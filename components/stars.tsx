3aceb0231f27ef18323ca6f3bc7f5a40afbb6476
IlyaTaidi <65737081+IlyaTaidi@users.noreply.github.com>
2022-02-21 16:55:16 -0500

    stars update

--- components/stars.tsx
+++ components/stars.tsx
@@ -7,49 +7,19 @@ const Stars = () =>
         options={{
             background: {
                 color: {
-                    value: "#0d47a1",
+                    value: "#000000",
                 },
             },
             fpsLimit: 60,
             interactivity: {
                 detectsOn: "canvas",
-                events: {
-                    onClick: {
-                        enable: true,
-                        mode: "push",
-                    },
-                    onHover: {
-                        enable: true,
-                        mode: "repulse",
-                    },
-                    resize: true,
-                },
-                modes: {
-                    bubble: {
-                        distance: 400,
-                        duration: 2,
-                        opacity: 0.8,
-                        size: 40,
-                    },
-                    push: {
-                        quantity: 4,
-                    },
-                    repulse: {
-                        distance: 200,
-                        duration: 0.4,
-                    },
-                },
             },
             particles: {
                 color: {
                     value: "#ffffff",
                 },
                 links: {
-                    color: "#ffffff",
-                    distance: 150,
-                    enable: true,
-                    opacity: 0.5,
-                    width: 1,
+                    enable: false,
                 },
                 collisions: {
                     enable: true,
@@ -57,17 +27,17 @@ const Stars = () =>
                 move: {
                     direction: "none",
                     enable: true,
-                    outMode: "bounce",
-                    random: false,
-                    speed: 6,
+                    outMode: "out",
+                    random: true,
+                    speed: 0.2,
                     straight: false,
                 },
                 number: {
                     density: {
                         enable: true,
-                        value_area: 800,
+                        value_area: 600,
                     },
-                    value: 80,
+                    value: 60,
                 },
                 opacity: {
                     value: 0.5,
@@ -76,8 +46,8 @@ const Stars = () =>
                     type: "circle",
                 },
                 size: {
-                    random: true,
-                    value: 5,
+                    random: false,
+                    value: 1,
                 },
             },
             detectRetina: true,
